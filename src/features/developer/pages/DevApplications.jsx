1b379a56f8912b5d22e74f1b601db9974deff8c2
Carlos Damota <cdamota.cd@gmail.com>
2025-04-28 11:17:42 +0200

    feat(pages): Implement base components and router structure

---
+++ src/features/developer/pages/DevApplications.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DevApplications = () => {
+  return (
+    <div>DevApplications</div>
+  )
+}
---
+++ src/features/recruiters/pages/RecProfile.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RecProfile = () => {
+  return (
+    <div>RecProfile</div>
+  )
+}
