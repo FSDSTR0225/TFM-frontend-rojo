316c8159790238aa418c9587ff86bc6071d6b748
Carlos Damota <cdamota.cd@gmail.com>
2025-05-05 19:42:29 +0200

    feat: implement initial state and fetch logic in OfferInfoPage component

--- src/features/recruiters/pages/OfferInfoPage.jsx
+++ src/features/recruiters/pages/OfferInfoPage.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { getOffersById } from '../../../services/offersServices'
 
 export const OfferInfoPage = () => {
+    const [offer, setOffer] = useState()
+     
+    useEffect(()=>{
+        const fetchOffer = async () => {
+            const offerData = await getOffersById() =>{
+                
+            }
+        }
+    })
 
   return (
     <div>OfferInfoPage</div>
