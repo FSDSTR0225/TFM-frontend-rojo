c4bb87011df0fe9b37021d1162af9513924f9165
antonioj-arce <antonioj.arce@hotmail.com>
2025-05-06 11:49:02 +0200

    comment some lines in approuter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
       { path: 'offers', element: <Offers />},
       { path: 'projects', element: <ProjectsPage /> },
       { path: 'projects/details/:projectid', element: <ProjectDetailsPage /> }, 
-      { path: 'developers/:developerid', element: <ProfileDevPage /> },
+      { path: 'profile/:developerid', element: <ProfileDevPage /> },
 
 
       //{ path: 'login', element: <Login /> },
@@ -47,7 +47,7 @@ const router = createBrowserRouter([
         //loader: authLoader,
         children: [
           //{ path: 'dashboard', element: <DeveloperDashboard />, loader: developerLoader },
-          { path: 'profile', element: <ProfileDevPage /> },
+          // { path: 'profile', element: <ProfileDevPage /> },
           { path: 'profile/edit', element: <EditDevProfilePage />},
           { path: 'projects', element: <ProjectsPage /> },
           { path: 'projects/new', element: <NewProjectPage /> },
@@ -66,7 +66,7 @@ const router = createBrowserRouter([
           { path: 'dashboard', element: <RecDashBoar />},
       
           // 2. Perfil de recruiter (ver y editar)
-          {path: 'profile', element: <RecProfile />},
+          // {path: 'profile', element: <RecProfile />},
           {path: 'profile/edit', element: <RecruiterEditForm /> },
       
           // 3. Gestión de ofertas
