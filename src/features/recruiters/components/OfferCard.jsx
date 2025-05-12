96fb48e3708921682eed1de1cdac68647d99e258
Carlos Damota <cdamota.cd@gmail.com>
2025-05-12 16:06:40 +0200

    Refactor MenuCard and OfferCard components to improve click handling and pass owner data; update OfferInfo and OfferList for consistent styling and layout adjustments.

--- src/features/recruiters/components/OfferCard.jsx
+++ src/features/recruiters/components/OfferCard.jsx
@@ -1,53 +1,86 @@
-import { Link } from "react-router";
-import { getDaysSince } from "../../../utils/utils";
+import { Link, useNavigate } from "react-router";
+import { capitalize, getDaysSince, getInitials } from "../../../utils/utils";
 import { TfiLocationPin } from "react-icons/tfi";
 import { MdOutlineAccessTime } from "react-icons/md";
-import { useState } from "react";
-import { TiThMenuOutline } from "react-icons/ti";
+
 import { MainRecButton } from "../../../components/MainRecButton";
 import { MenuCard } from "./MenuCard";
 
-export const OfferCard = ({ classProps, offer}) => {
-  const [openMenu, setOpenMenu] = useState(false)
+export const OfferCard = ({ classProps, offer, owner, id }) => {
+  const navigate = useNavigate()
 
-const handleOnClick = (e) => {
-  e.preventDefault();
-};
+const name = capitalize(owner?.name || '');
+const surname = capitalize(owner?.surname || '');
+const completeName = `${name} ${surname}`.trim() || 'Unknown Recruiter';
 
-
+  const handleOnClick = (e) => {
+    e.stopPropagation();
+  };
+  const handleCardClick = () => {
+    navigate(`/offers/${offer._id}`);
+  };
 
   const daysAgo = offer?.createdAt ? getDaysSince(offer?.createdAt) : 0;
   return (
-    <Link to={`/offers/${offer?._id}`}>
       <li
-        key={offer?._id}
+      key={id}
+        onClick={handleCardClick}
         className={`${
           classProps && classProps
-        } card border bg-neutral-80 border-neutral-70 cursor-pointer max-h-80 shadow-xl hover:bg-neutral-90`}
+        } card border bg-neutral-80 border-neutral-70 cursor-pointer max-h-80 shadow-xl hover:bg-neutral-90 transition-transform transform hover:scale-105 `}
       >
         <div className='card-body'>
-        <MenuCard onClick={handleOnClick} />
-        
-        <div className='flex flex-col justify-between'>
-              <h3 className='text-xl font-bold'>{offer?.position}</h3>
-              <p className="text-neutral-10">{offer?.role}</p>
+        <div className="flex justify-between">
+        <div className='avatar gap-2 items-center'>
+            {owner?.role?.recruiter?.logo ? (
+              <div className='avatar'>
+                <div className='size-8 sm:size-8 rounded-full'>
+                  <img
+                    src={owner.role.recruiter.logo}
+                    alt='Logo'
+                  />
+                </div>
               </div>
-                  <div className="flex gap-4">
-                  <div className='flex items-center gap-2'><TfiLocationPin />{offer.location}</div>
-                  <div className="badge text-neutral-0 bg-neutral-60">{offer?.contractType}</div>
-                  </div>
-                  <p className='line-clamp-3'>{offer.description}</p>
-                  <div className="flex items-center justify-end gap-4 m-2">
-                    <MainRecButton classProps='rounded-full w-18'>Apply</MainRecButton>
-                    <MainRecButton classProps='rounded-full w-18 text-secondary-40 bg-transparent'>Contact</MainRecButton>
-                  </div>
-                  
-                  <div className=' flex  items-center text-neutral-20 text-xs'>
-                    <p className='flex items-center gap-2 '><MdOutlineAccessTime />Posted {daysAgo} day{daysAgo !== 1 ? "s" : ""} ago</p>
-                    <span>{offer.applicants ? `${offer.applicants.length} Aplicants` : "0 Aplicants"}</span>
-                  </div>
+            ) : (
+              <div className='avatar avatar-placeholder'>
+                <div className='bg-neutral text-neutral-content rounded-full size-8 sm:size-'>
+                  <span className=' font-bold'>{getInitials(completeName)}</span>
+                </div>
+              </div>
+            )}
+            <p>{completeName}</p>
+          </div>
+          <MenuCard onClick={handleOnClick}/>
+        </div>
+          
+
+          <div className='flex flex-col justify-between'>
+            <h3 className='text-xl font-bold'>{offer?.position}</h3>
+            <p className='text-neutral-10'>{offer?.role}</p>
+          </div>
+          <div className='flex gap-4'>
+            <div className='flex items-center gap-2'>
+              <TfiLocationPin />
+              {offer.location}
+            </div>
+            <div className='badge text-neutral-0 bg-neutral-60'>{offer?.contractType}</div>
+          </div>
+          <p className='line-clamp-3'>{offer.description}</p>
+          <div className='flex items-center justify-end gap-4 m-2'>
+            <MainRecButton onClick={handleOnClick} classProps='rounded-full w-18'>Apply</MainRecButton>
+            <MainRecButton onClick={handleOnClick} classProps='rounded-full w-18 hover:border-neutral-0 hover:text-neutral-0'>
+              Contact
+            </MainRecButton>
+          </div>
+
+          <div className=' flex  items-center text-neutral-20 text-xs'>
+            <p className='flex items-center gap-2 '>
+              <MdOutlineAccessTime />
+              Posted {daysAgo} day{daysAgo !== 1 ? "s" : ""} ago
+            </p>
+            <span>{offer.applicants ? `${offer.applicants.length} Aplicants` : "0 Aplicants"}</span>
+          </div>
         </div>
       </li>
-    </Link>
   );
 };
