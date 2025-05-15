006622c5be62bfcdd9085a099e007fa5ca7a4bf0
Jeremie <jeremiechiclote34@gmail.com>
2025-05-15 23:57:58 +0200

    solucion dev

--- src/features/recruiters/pages/RecProfile.jsx
+++ src/features/recruiters/pages/RecProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useState,useContext, useEffect } from 'react'
 import { OfferCard } from '../components/OfferCard'
 import { OfferModal } from '../components/OfferModal'
 import { AuthContext } from '../../../context/authContext';
