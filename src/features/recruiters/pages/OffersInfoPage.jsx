45bdcef6f5c77f4165bbc0f4d58757c4796efa2f
Carlos Damota <cdamota.cd@gmail.com>
2025-05-20 16:40:07 +0200

    Refactor OffersInfoPage and FilterOffers components; integrate filtering functionality, enhance UI with icons, and streamline state management for contract and skills filters.

--- src/features/recruiters/pages/OffersInfoPage.jsx
+++ src/features/recruiters/pages/OffersInfoPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { useState } from 'react'
 import { getOffers } from '../../../services/offersServices'
 import { useEffect } from 'react'
@@ -7,7 +7,8 @@ import { SectionContainer } from '../../../components/SectionContainer';
 import {OfferList} from '../components/OfferList'
 import { Pagination } from '../../../components/Pagination';
 import { ModalDelete } from '../components/ModalDelete';
-import { PiFunnel, PiFunnelX, PiSortAscending, PiSortDescending } from 'react-icons/pi';
+
+import { FilterOffers } from '../components/FilterOffers';
 
 
 
@@ -109,117 +110,40 @@ const getFilteredOffers = () => {
         }
       if (error) return <p>Error al cargar las ofertas: {error}</p>;
 
-const contractsTypes = [...new Set(offers.flatMap(offer =>offer.contractType.map(type => type.trim())))]
-console.log("🚀 ~ OffersInfoPage ~ contractsTypes:", contractsTypes)
-const hardSkills = [
-  ...new Set(
-    offers.flatMap(offer =>
-      offer.skills
-        .flatMap(s => s.split(","))
-        .map(s => s.trim())
-        .filter(Boolean) // Elimina vacíos
-    )
-  )
-];
-console.log("🚀 ~ OffersInfoPage ~ hardSkills:", hardSkills)
+// const contractsTypes = [...new Set(offers.flatMap(offer =>offer.contractType.map(type => type.trim())))]
+
+// const hardSkills = [
+//   ...new Set(
+//     offers.flatMap(offer =>
+//       offer.skills
+//         .flatMap(s => s.split(","))
+//         .map(s => s.trim())
+//         .filter(Boolean) // Elimina vacíos
+//     )
+//   )
+// ];
+
 
 
 
 
   return (
+
     <SectionContainer>
         <h2 className='text-3xl font-bold text-neutral-0'>Your Next Tech Career Starts Here</h2>
         <p className='text-neutral-10 text-lg '>Discover job opportunities for developers, designers, and engineers in fast-growing tech fields.</p>
 
-      {/* change popover-1 and --anchor-1 names. Use unique names for each dropdown */}
-      <div className='flex items-center gap-4 xl:-mb-8 mt-6'>
-<div className='relative'>
-<label className="btn btn-md swap swap-rotate w-25">
-  <input
-    type="checkbox"
-    checked={filtersOpen}
-    onChange={() => setFiltersOpen(prev => !prev)}
-  />
-  <div className="swap-on flex items-center gap-2">
-    <PiFunnelX className="size-5" /> Filters
-  </div>
-  <div className="swap-off flex items-center gap-2">
-    <PiFunnel className="size-5" /> Filters
-  </div>
-</label>
-
-{/* selectores del filtro */}
-{filtersOpen && (
-  <ul className="absolute top-12  z-50 rounded-box bg-neutral-90 shadow-lg border-2 border-neutral-70 min-w-85">
-    <li className="flex flex-col p-4 border-b-2 border-neutral-50">
-      <label>
-        Type Contract:
-        
-      </label>
-      <select
-      className='p-2 mt-2 bg-neutral-90 border-2 border-neutral-70'
-          value={contractTypeFilter}
-          onChange={(e) => setContractTypeFilter(e.target.value)}
-        >
-          <option value="" className='bg-neutral-90 p-2'>All</option>
-          {contractsTypes.map((contract, index) => (
-            <option className='bg-neutral-90 p-2' key={index} value={contract}>
-              {contract}
-            </option>
-          ))}
-        </select>
-    </li>
-
-    <li className="flex flex-col flex-wrap p-4 border-b-2 border-neutral-50">
-      <fieldset className="flex items-center gap-2 flex-wrap">
-        <legend className='mb-2'>Skills:</legend>
-        {hardSkills.map((skill) => (
-          <label className="flex items-center  cursor-pointer " key={skill}>
-            <input
-            className='btn btn-sm btn-outline btn-secondary-40 rounded-full'
-            aria-label={skill}
-              type="checkbox"
-              checked={skillsFilter.includes(skill)}
-              onChange={(e) => {
-                const checked = e.target.checked;
-                setSkillsFilter((prev) =>
-                  checked ? [...prev, skill] : prev.filter((s) => s !== skill)
-                );
-              }}
-            />
-            {/* <span className="bg-neutral-70 border border-neutral-60 px-3 py-1 rounded-full shadow-xl ">{skill}</span> */}
-          </label>
-        ))}
-      </fieldset>
-    </li>
-
-    <li className="p-4">
-      <button
-        onClick={resetFilters}
-        className="btn btn-outline btn-error w-full"
-      >
-        Reset Filters
-      </button>
-    </li>
-  </ul>
-)}</div>
-
-
-        
-
-        
-        
-
-          <label className="btn btn-md swap swap-rotate w-25">
-  <input  
-    type="checkbox"
-    checked={sortOrder === 'desc'} // ✅ Activa visualmente el swap
-    onChange={() => setSortOrder((prev) => (prev === 'asc' ? 'desc' : 'asc'))}
-  />
-  <div className="swap-on flex inline-center gap-2"><PiSortAscending className='size-5' />Latest</div>
-  <div className="swap-off flex inline-center gap-2"><PiSortDescending className='size-5'/>Oldest</div>
-</label>
-  </div>      
+      <FilterOffers         offers={offers}
+        filtersOpen={filtersOpen}
+        setFiltersOpen={setFiltersOpen}
+        contractTypeFilter={contractTypeFilter}
+        setContractTypeFilter={setContractTypeFilter}
+        skillsFilter={skillsFilter}
+        setSkillsFilter={setSkillsFilter}
+        sortOrder={sortOrder}
+        setSortOrder={setSortOrder}
+        resetFilters={resetFilters}/>
+     
 
 
 
