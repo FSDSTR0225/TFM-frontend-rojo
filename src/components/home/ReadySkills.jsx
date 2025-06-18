b9c5f52c71a6af729e6a30625d574285a791ba61
antonioj-arce <antonioj.arce@hotmail.com>
2025-06-18 11:31:06 +0200

    editing homepage

---
+++ src/components/home/ReadySkills.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export const ReadySkills = () => {
+
+  return (
+        <div className="w-full bg-gradient-to-r from-secondary-90 to-primary-90 rounded-3xl py-16">
+            <div className="flex flex-col">
+                <div className='flex flex-col items-center text-center mb-4'>
+                    <h1 className="bg-gradient-to-r from-primary-50 to-secondary-50 text-transparent bg-clip-text leading-normal inline-block text-5xl font-bold mb-2">Ready to show off your skills?</h1>
+                    <span className='text-neutral-20'>Create your developer profile and connect with top tech companies.</span>
+                </div>
+                <div className='flex flex-row items-center justify-center text-center mt-4'>
+                    <Link to={`/offers`} className="btn bg-gradient-to-r from-secondary-50 to-primary-50 rounded-md gap-1 mr-4">
+                        Create a new Project
+                    </Link>
+                    <Link to={`/offers`} className="btn bg-gradient-to-r from-primary-50 to-secondary-50 rounded-md gap-1 ml-4">
+                        Create a new Offer
+                    </Link>
+                </div>
+            </div>
+        </div>
+  );
+};
