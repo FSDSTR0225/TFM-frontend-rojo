823ad640ea75def5c4957328053a1b6d714cd651
Beliochando <beliochando@gmail.com>
2025-06-27 22:10:49 +0200

    fixed badge in offercard to build

--- src/features/recruiters/components/OfferCard.jsx
+++ src/features/recruiters/components/OfferCard.jsx
@@ -7,7 +7,7 @@ import { MenuCard } from "./MenuCard";
 import { useContext } from "react";
 import { AuthContext } from "../../../context/authContext";
 import { PiMapPinArea } from "react-icons/pi";
-import Badge from "../../../components/Badge";
+import Badge from "../../../components/Badge.jsx";
 import { AvatarImage } from "../../../components/AvatarImage";
 import { NameUsers } from "../../../components/NameUsers";
 import { ChatContext } from "../../../layout/chat/context/ChatContext";
@@ -22,7 +22,6 @@ export const OfferCard = ({
   setSelectedOfferId,
   isOpenModalEdit,
   setIsOpenModalEdit,
-
 }) => {
   const navigate = useNavigate();
   const { profile, token } = useContext(AuthContext);
@@ -31,9 +30,7 @@ export const OfferCard = ({
 
   const isRecruiter = profile?.role.type === "recruiter";
 
-  const {openChat} = useContext(ChatContext);
-
-
+  const { openChat } = useContext(ChatContext);
 
   //Determinando en donde este redirije a un lado o al otro dependiendo de la ruta actual
   const location = useLocation();
@@ -42,13 +39,13 @@ export const OfferCard = ({
     e.stopPropagation();
     if (!token) {
       console.log("por aqui no pasaras");
-     navigate("/login", { state: { from: "/offers" } });
+      navigate("/login", { state: { from: "/offers" } });
     }
 
     setIsOpenApplyModal(true);
     setSelectedOfferId(offer?._id);
   };
-  
+
   // const handleApply = async (e) => {
   //   e.stopPropagation();
   //   if (!token) {
@@ -87,22 +84,22 @@ export const OfferCard = ({
     e.stopPropagation();
   };
 
-    const handleContactClick = (e) => {
+  const handleContactClick = (e) => {
     e.stopPropagation();
-      openChat(offer.owner);
+    openChat(offer.owner);
   };
   const handleCardClick = () => {
-    if (location.pathname.startsWith('/private-rec/offers')) {
+    if (location.pathname.startsWith("/private-rec/offers")) {
       navigate(`/private-rec/dashboard/${offer._id}`);
     } else {
       navigate(`/offers/${offer._id}`);
     }
   };
 
-   const handleOwnerClick = (e) => {
+  const handleOwnerClick = (e) => {
     e.stopPropagation();
-    navigate(`../../recruiter/${offer.owner._id}`)
-   }
+    navigate(`../../recruiter/${offer.owner._id}`);
+  };
 
   const daysAgo = offer?.createdAt ? getDaysSince(offer?.createdAt) : 0;
   return (
@@ -113,11 +110,11 @@ export const OfferCard = ({
         classProps && classProps
       } card border bg-neutral-80 border-neutral-70 cursor-pointer max-h-80 shadow-xl hover:bg-neutral-90 transition-transform transform hover:scale-105 `}
     >
-      <div className='card-body justify-between'>
-        <div className='flex justify-between items-center'>
+      <div className="card-body justify-between">
+        <div className="flex justify-between items-center">
           <div className="flex items-center gap-2 " onClick={handleOwnerClick}>
-          <AvatarImage user={offer.owner} width={8} />
-          <NameUsers user={offer.owner} classProps={"text-xs"}/>
+            <AvatarImage user={offer.owner} width={8} />
+            <NameUsers user={offer.owner} classProps={"text-xs"} />
           </div>
           {isOwner && isRecruiter && (
             <MenuCard
@@ -131,45 +128,51 @@ export const OfferCard = ({
           {hasApplied && <Badge text={"Applied"} />}
         </div>
 
-        <div className='flex flex-col justify-between'>
-          <h3 className='text-xl font-bold'>{offer?.position}</h3>
-          <p className='text-neutral-10'>{offer?.role}</p>
+        <div className="flex flex-col justify-between">
+          <h3 className="text-xl font-bold">{offer?.position}</h3>
+          <p className="text-neutral-10">{offer?.role}</p>
         </div>
-        <div className='flex gap-4'>
-          <div className='flex items-center gap-2'>
-            <PiMapPinArea className='size-4' />
+        <div className="flex gap-4">
+          <div className="flex items-center gap-2">
+            <PiMapPinArea className="size-4" />
             {offer.location}
           </div>
-          <div className='badge text-neutral-0 bg-neutral-60'>{offer?.contractType}</div>
+          <div className="badge text-neutral-0 bg-neutral-60">
+            {offer?.contractType}
+          </div>
         </div>
         <div>
-          <p className='line-clamp-3'>{offer?.description}</p>
+          <p className="line-clamp-3">{offer?.description}</p>
         </div>
 
         {!isOwner && !isRecruiter && (
-          <div className='flex items-center justify-end gap-4 m-2'>
+          <div className="flex items-center justify-end gap-4 m-2">
             <MainRecButton
               onClick={handleApplyModal}
-              classProps='rounded-full w-18'
+              classProps="rounded-full w-18"
               disabled={hasApplied}
             >
               {hasApplied ? "Applied" : "Apply"}
             </MainRecButton>
             <MainRecButton
               onClick={handleContactClick}
-              classProps='rounded-full w-18 hover:border-neutral-0 hover:text-neutral-0 text-secondary-40 hover:bg-transparent bg-transparent'
+              classProps="rounded-full w-18 hover:border-neutral-0 hover:text-neutral-0 text-secondary-40 hover:bg-transparent bg-transparent"
             >
               Contact
             </MainRecButton>
           </div>
         )}
 
-        <div className=' flex  items-center text-neutral-20 text-xs'>
-          <p className='flex items-center gap-2 '>
+        <div className=" flex  items-center text-neutral-20 text-xs">
+          <p className="flex items-center gap-2 ">
             <MdOutlineAccessTime />
             Posted {daysAgo} day{daysAgo !== 1 ? "s" : ""} ago
           </p>
-          <span>{offer?.applicants ? `${offer?.applicants.length} Aplicants` : "0 Aplicants"}</span>
+          <span>
+            {offer?.applicants
+              ? `${offer?.applicants.length} Aplicants`
+              : "0 Aplicants"}
+          </span>
         </div>
       </div>
     </li>
