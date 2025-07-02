ef49c3cff5bbfea08922a018449bac80ed9f1196
Carlos Damota <cdamota.cd@gmail.com>
2025-07-02 23:14:02 +0200

    Refactor AvatarImage component: update avatar styling for improved visual consistency and user experience

--- src/components/AvatarImage.jsx
+++ src/components/AvatarImage.jsx
@@ -44,7 +44,7 @@ const completeName = `${name} ${surname}`.trim() || 'Unknown Recruiter';
   return (
     < >
      {( user?.avatar) ? (      
-                    <div className={`avatar border-2 border-neutral-90 rounded-full  ${userOnline && (isDeveloper ? 'avatar-online before:w-[20%] before:h-[20%] before:top-[6%] before:right-[2%] before:outline-none before:bg-primary-50 border-primary-50 ' : 'before:w-[20%] before:h-[20%] before:top-[6%] before:right-[2%] border-secondary-50 avatar-online before:outline-none before:bg-secondary-50')}`}>
+                    <div className={`avatar outline-2 outline-neutral-90 rounded-full  ${userOnline && (isDeveloper ? 'avatar-online before:w-[20%] before:h-[20%] before:top-[6%] before:right-[2%]  before:bg-primary-50 outline-primary-50 ' : 'before:w-[20%] before:h-[20%] before:top-[6%] before:right-[2%] outline-secondary-50 avatar-online  before:bg-secondary-50')}`}>
                       <div className={`rounded-full ${sizeClass} `}>
                         <img
                           src={ user?.avatar}
@@ -53,7 +53,7 @@ const completeName = `${name} ${surname}`.trim() || 'Unknown Recruiter';
                       </div>
                     </div>
                   ) : (
-                    <div className={`avatar avatar-placeholder ${userOnline && 'avatar-online' }`}>
+                    <div className={`avatar avatar-placeholder outline-2 outline-neutral-90 rounded-full  ${userOnline && (isDeveloper ? 'avatar-online before:w-[20%] before:h-[20%] before:top-[6%] before:right-[2%]  before:bg-primary-50 outline-primary-50 ' : 'before:w-[20%] before:h-[20%] before:top-[6%] before:right-[2%] outline-secondary-50 avatar-online  before:bg-secondary-50')}`}>
                       <div className={`bg-neutral text-neutral-content rounded-full ${sizeClass}`}>
                         <span className=' font-bold'>{getInitials(completeName)}</span>
                       </div>
