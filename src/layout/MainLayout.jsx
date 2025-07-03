b4961e18c2ca63e8947e92a096c48261084faee2
Carlos Damota <cdamota.cd@gmail.com>
2025-07-03 22:08:38 +0200

    Refactor layout components: enhance styling for improved responsiveness and visual consistency

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -6,13 +6,13 @@ import { Widget } from "./chat/Widget"
 export const MainLayout= () => {
     return (
         <>
-            <div className="flex flex-col min-h-[100dvh] ">
+            
                <Header />
-                <main className="flex-grow bg-neutral-90">
+                <main className=" bg-neutral-90">
                     {<Outlet />}
                 </main>
             <Footer />
-            </div>
+           
 
             {/* Chat flotante en toda la web */}
             <Widget />
