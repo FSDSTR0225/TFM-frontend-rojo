b2fb05dacfc30489fd135c46dbc3b790a65e4b1b
Jeremie <jeremiechiclote34@gmail.com>
2025-07-03 19:27:26 +0200

    Notificaciones chat

--- src/layout/chat/context/ChatContext.jsx
+++ src/layout/chat/context/ChatContext.jsx
@@ -1,40 +1,44 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { createContext } from 'react';
-
+import { AuthContext } from "../../../context/authContext";
 export const ChatContext = createContext();
-export const ChatProvider = ({children}) => {
-    const [isChatOpen, setIsChatOpen] = useState(false);
-    const [selectedUser, setSelectedUser] = useState(null);
-    const [isOpen, setIsOpen] = useState(false);
-    const [screen, setScreen] = useState("welcome");
+export const ChatProvider = ({ children }) => {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [screen, setScreen] = useState("welcome");
+  const { setNotifications } = useContext(AuthContext);
 
-     const toggleChat = () => {
+  const toggleChat = () => {
     setIsOpen(!isOpen);
   };
 
   const backToWelcome = () => {
-  setScreen("welcome");
-  setSelectedUser(null);
-}
-    const openChat = (user) => {
-        toggleChat();
-        setScreen("chat");
-        setSelectedUser(user);
-        setIsChatOpen(true);
-    };
+    setScreen("welcome");
+    setSelectedUser(null);
+  }
+  const openChat = (user) => {
+    toggleChat();
+    setScreen("chat");
+    setSelectedUser(user);
+    setIsChatOpen(true);
+  };
 
-    const closeChat = () => {
-        setSelectedUser(null);
-        setIsChatOpen(false);
-    };
+  const closeChat = () => {
+    setSelectedUser(null);
+    setIsChatOpen(false);
+  };
 
-    const handleSelectedUser = (usuario) => {
+  const handleSelectedUser = (usuario) => {
+    setNotifications((prev) =>
+      prev.filter(n => String(n.senderId) !== String(usuario._id))
+    );
     setScreen("chat");
     setSelectedUser(usuario);
   }
 
   return (
-    <ChatContext.Provider value={{ isChatOpen, selectedUser, setSelectedUser, openChat, closeChat, toggleChat, isOpen, backToWelcome, handleSelectedUser, screen}}>
+    <ChatContext.Provider value={{ isChatOpen, selectedUser, setSelectedUser, openChat, closeChat, toggleChat, isOpen, backToWelcome, handleSelectedUser, screen }}>
       {children}
     </ChatContext.Provider>
   )
